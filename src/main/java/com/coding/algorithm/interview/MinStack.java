132328be2fe301027a967b7180d05cd66c9d42a6
mengxz <82427129@qq.com>
2017-09-28 10:41:34 +0800

    minstack comparable comparator

--- src/main/java/com/coding/algorithm/interview/MinStack.java
+++ src/main/java/com/coding/algorithm/interview/MinStack.java
@@ -1,25 +1,45 @@
 package com.coding.algorithm.interview;
 
+import java.util.Comparator;
 import java.util.Stack;
+
+import com.coding.algorithm.sort.Utils;
 
 /**
  * 最小栈，可以O(1)获取最小值的栈
  *
  * @param <T>
  */
-public class MinStack<T extends Comparable<T>> {
+public class MinStack<T> {
 	private Stack<T> stackData = new Stack<T>();
 	private Stack<T> stackMin = new Stack<T>();
 
+	private Comparator<T> cpt;
+
 	public MinStack() {
+		cpt = null;
 	}
 
+	public MinStack(Comparator<T> cpt) {
+		this.cpt = cpt;
+	}
+
+	@SuppressWarnings("unchecked")
 	public void push(T obj) {
 		stackData.push(obj);
-		if(stackMin.isEmpty()||stackMin.peek().compareTo(obj)>0){
-			stackMin.push(obj);
-		}else{
-			stackMin.push(stackMin.peek());
+		if (cpt != null) {
+			if (stackMin.isEmpty() || cpt.compare(stackMin.peek(), obj) > 0) {
+				stackMin.push(obj);
+			}else{
+				stackMin.push(stackMin.peek());
+			}
+		} else {
+			if (stackMin.isEmpty()
+					|| ((Comparable<T>) stackMin.peek()).compareTo(obj) > 0) {
+				stackMin.push(obj);
+			} else {
+				stackMin.push(stackMin.peek());
+			}
 		}
 	}
 
@@ -31,10 +51,11 @@ public class MinStack<T extends Comparable<T>> {
 	public T getMin() {
 		return stackMin.peek();
 	}
-	
+
 	@Override
 	public String toString() {
-		return "data:"+stackData.toString()+"\n"+"min:"+stackMin.toString();
+		return "data:" + stackData.toString() + "\n" + "min:"
+				+ stackMin.toString();
 	}
 
 	public static void main(String[] args) {
@@ -46,6 +67,16 @@ public class MinStack<T extends Comparable<T>> {
 		stack.push(7);
 		stack.push(0);
 		System.out.println(stack);
+	}
+	
+	class MyObjectComparator implements Comparator<Utils>{
+
+		@Override
+		public int compare(Utils o1, Utils o2) {
+			
+			return 0;
+		}
+		
 	}
 
 }
